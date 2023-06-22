18ea49ae9746b3e6d8c25fb45abb34831bc90191
cninz <nchua.nc47@gmail.com>
2023-06-22 16:46:40 -0600

    temporary fix js script

--- js/script.js
+++ js/script.js
@@ -22,27 +22,6 @@ $(function () {
     });
 
     
-$(function () {
-    $(".carousel").carousel({ interval: 3000, pause: false, wrap: true,  });
-    $("#carouselButton").on('click', function () {
-        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        } else {
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
-        }
-    });
-
-    $("#reserveButton").on('click', function () {
-        $('#reserveModal').modal('show');
-    });
- 
-    $("#loginButton").on('click',function () {
-        $('#loginModal').modal('show');
-    });
 
     const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
     navLinks.forEach(link => {
@@ -56,4 +35,4 @@ $(function () {
 });
 
     
-});
+
