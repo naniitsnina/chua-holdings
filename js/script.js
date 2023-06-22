cc95318fa4b708f35c4dec500069a22d3fd7fb6c
cninz <nchua.nc47@gmail.com>
2023-06-22 11:55:21 -0600

    Update-Mobile Friendly Edit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 $(function () {
-    $(".carousel").carousel({ interval: 2000, pause: false });
+    $(".carousel").carousel({ interval: 3000, pause: false, wrap: true,  });
     $("#carouselButton").on('click', function () {
         if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
@@ -20,6 +20,40 @@ $(function () {
     $("#loginButton").on('click',function () {
         $('#loginModal').modal('show');
     });
+
     
+$(function () {
+    $(".carousel").carousel({ interval: 3000, pause: false, wrap: true,  });
+    $("#carouselButton").on('click', function () {
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+
+    $("#reserveButton").on('click', function () {
+        $('#reserveModal').modal('show');
+    });
+ 
+    $("#loginButton").on('click',function () {
+        $('#loginModal').modal('show');
+    });
+
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    });    
+});
+
     
 });
